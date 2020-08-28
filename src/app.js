d9d99b6718265eb5251b5d249aa98382c62de1ff
Lindsay Esterman <lindsayesterman@gmail.com>
2020-08-28 19:24:06 -0400

    get and post endpoints

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const winston = require('winston');
+const { v4: uuid } = require('uuid');
 
 const app = express()
 
@@ -18,8 +19,8 @@ const bookmarks = [{
     url: 'https://bookmarks-app-snowy.vercel.app/add-bookmark',
     description: 'This is bookmark one',
     rating: '5'
-  }];
-  
+}];
+
 
 const logger = winston.createLogger({
     level: 'info',
@@ -38,27 +39,78 @@ if (NODE_ENV !== 'production') {
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-  
-
+    
+    
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         logger.error(`Unauthorized request to path: ${req.path}`);
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     // move to the next middleware
     next()
-  })
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.get('/bookmarks', (req, res) => {
+app.get('/bookmark', (req, res) => {
     res
     .json(bookmarks)
 });
+
+app.get('/bookmark/:id', (req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(c => c.id == id);
+    
+    if (!bookmark) {
+        logger.error(`Bookmark with id ${id} not found.`);
+        return res
+        .status(404)
+        .send('Not Found');
+    }
+    
+    res.json(bookmark);
+});
+
+app.post('/bookmark', (req,res) => {
+    const { title, url, description, rating } = req.body;
+    
+    if  (!title) {
+        logger.error(`title is required`);
+        return res
+        .status(400)
+        .send('Invalid data');
+    }
+    
+    if (!url) {
+        logger.error(`url is required`);
+        return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    const id = uuid();
+    
+    const bookmark = {
+        id,
+        title,
+        url, 
+        description,
+        rating
+    };
+    
+    bookmarks.push(bookmark);
+    
+    logger.info(`Bookmark with id ${id} created`);
+    res
+    .status(201)
+    .location(`http://localhost:8000/bookmark/${id}`)
+    .json(bookmark);
+})
 
 app.use(function errorHandler(error, req, res, next) {
     let response
